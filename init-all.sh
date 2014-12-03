d498ee9f2a69a2e60293fa3e2da7b005a1ccc38f
Remi Somdecoste-Lespoune <saponace@gitbucket.org>
2014-12-03 23:20:02 +0100

    continue le boulot, encore assez loi du but

--- init-all.sh
+++ init-all.sh
@@ -12,6 +12,15 @@ usage(){
 }
 
 
+all(){
+    installYaourt()
+    installSpf13()
+    installApache()
+    installNetworkManager()
+    installPackages()
+    sudo chsh -s /bin/zsh saponace                           # change default shell to ZSH
+}
+
 
 intallYaourt(){
     echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
@@ -19,17 +28,40 @@ intallYaourt(){
 }
 
 
+installSpf13(){
+    sudo curl http://j.mp/spf13-vim3 -L -o - | sh       # spf13, config and plugin pack
+}
+
+
+installApache(){
+    pacman -S apache
+    pacman -S php php-apache
+    pacman -S mysql
+    # Allow the use of PHP in apache
+    echo -e "# Use for PHP 5.x:\nLoadModule php5_module       modules/libphp5.so\n" >> /etc/httpd/conf/httpd.conf
+    ehco -e "AddHandler php5-script php\nInclude conf/extra/php5_module.conf" >> /etc/http/conf/httpd.conf
+    sed -i "s/mpm_event/mpm_prefork/g" /etc/httpd/conf/httpd.conf
+}
+
+
+# Network manager
+installNetworkManager(){
+    pacman -S networkmanager 
+    systemctl enable NetworkManager
+    pacman -S network-manager-applet gnome-keyring gnome-icon-theme
+    #(si marche pas, désactiver ipv6 dans dhcpcp)
+}
+
+
+
+
 insatllPackages(){
 
 }
 
-sudo chsh -s /bin/zsh saponace                           # change default shell to ZSH
 
 
 
-installSpf13(){
-    sudo curl http://j.mp/spf13-vim3 -L -o - | sh       # spf13, config and plugin pack
-}
 
 
 
