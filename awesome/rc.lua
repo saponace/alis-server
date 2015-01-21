61a2a018a96c8fe8aa71e9d0598dd9106e4f9a09
Remi Somdecoste-Lespoune <saponace@gitbucket.org>
2015-01-21 02:17:21 +0100

    fonction inutile commentée

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -40,14 +40,14 @@ end
 -- }}}
 
 -- {{{ Autostart applications
-function run_once(cmd)
-  findme = cmd
-  firstspace = cmd:find(" ")
-  if firstspace then
-     findme = cmd:sub(0, firstspace-1)
-  end
-  awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
-end
+--function run_once(cmd)
+  --findme = cmd
+  --firstspace = cmd:find(" ")
+  --if firstspace then
+     --findme = cmd:sub(0, firstspace-1)
+  --end
+  --awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+--end
 
 -- }}}
 
