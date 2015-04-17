e31f14f20bd4b055422180aadb4a7b607fb1a4be
root <saponace@gmail.com>
2015-04-17 21:12:46 +0200

    trois fois rien

--- init-before-reboot.sh
+++ init-before-reboot.sh
@@ -2,10 +2,10 @@
 
 hostname=($1)
 
-locale-gen
-ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
-hwclock --systohc --localtime
 echo  $hostname > /etc/hostname
+ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
+locale-gen
+hwclock --systohc --localtime
 systemctl enable dhcpcd.service
 mkinitcpio -p linux
 pacman -S grub os-prober
