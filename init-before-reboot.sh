914e3b36a5f865cee19538f02678b27b1715bee5
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-11-23 17:38:42 +0100

    cleaned and commented code

--- init-before-reboot.sh
+++ init-before-reboot.sh
@@ -5,14 +5,27 @@ username=($2)
 uefipart=($3)
 rootpart=($4)
 
-echo  $hostname > /etc/hostname
-ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
-locale-gen
-echo "KEYMAP=fr-latin1.map.gz" > /etc/vconsole.conf
-hwclock --systohc --localtime
-mkinitcpio -p linux
 
-# Boot manager
+
+# Set the hostname
+    echo  $hostname > /etc/hostname
+
+
+# Set le locales and the keymap
+    ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
+    locale-gen
+    echo "KEYMAP=fr-latin1.map.gz" > /etc/vconsole.conf
+
+
+# Set the clock
+    hwclock --systohc --localtime
+
+
+# Create ramdisk
+    mkinitcpio -p linux
+
+
+# Configure boot manager
     pacman -S efibootmgr
     mount $uefipart /boot
     bootctl --path=/boot install
@@ -22,20 +35,22 @@ mkinitcpio -p linux
     echo -e "title  arch\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=UUID=$rootpart_uuid rw" > /boot/loader/entries/arch.conf
 
 
+# Set root password
+    echo Enter root password:
+    passwd root
 
-echo enter root password:
-passwd root
 
 # add the user to wheel group (sudoers)
     useradd -m -G wheel -s /bin/bash $username
     sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" /etc/sudoers
-echo enter $username password:
-passwd $username
+    # Set user password
+        echo Enter $username password:
+        passwd $username
 
 
 # Move the git repo into the user's home directory
     pushd `dirname $0` > /dev/null
     git_repo_path=`pwd`
     popd > /dev/null
-    mv git_repo_path /home/$username
+    mv $git_repo_path /home/$username
     chown -R $username:$username /home/$username/
