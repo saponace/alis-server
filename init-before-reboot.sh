a1ad83c6ad359615cabb44a7eef8ee3bd009f122
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-11-23 16:53:26 +0100

    just testing, should work

--- init-before-reboot.sh
+++ init-before-reboot.sh
@@ -3,30 +3,29 @@
 hostname=($1)
 username=($2)
 uefipart=($3)
+rootpart=($4)
 
 echo  $hostname > /etc/hostname
 ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
 locale-gen
 localectl set-keymap fr-latin1.map.gz
 hwclock --systohc --localtime
-systemctl enable dhcpcd.service
+#systemctl enable dhcpcd.service
 mkinitcpio -p linux
 
 # Boot manager
-            #pacman -S grub os-prober
-            #grub-install --target=i386-pc --recheck /dev/sda
-            #grub-mkconfig -o /boot/grub/grub.cfg
     pacman -S efibootmgr
-    umount /sys/firmware/efi/efivars
-    mount -t efivarfs efivarfs /sys/firmware/efi/efivars
-    bootctl --path=/boot/ install
-    mkdir /boottemp
-    cp boot/{vmlinuz-linux,initramfs-linux.img} boottemp
+    #umount /sys/firmware/efi/efivars
+    #mount -t efivarfs efivarfs /sys/firmware/efi/efivars
     mount $uefipart /boot
-    cp boottemp/* boot/
-    rm -rf /boottemp
+    bootctl --path=/boot install
+    #mkdir /boottemp
+    #cp boot/{vmlinuz-linux,initramfs-linux.img} boottemp
+    #mv boottemp/* boot/
+    #rm -rf /boottemp
     echo -e "timeout=3\ndefault=arch" > /boot/loader/loader.conf
-    echo -e "title  arch\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=UUID=____LINUX_PARTITION_UUID_HERE____ rw" > /boot/loader/entries/arch.conf
+    rootpart_uuid=$(blkid $rootpart | cut -f2 -d\")
+    echo -e "title  arch\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=UUID=$rootpart_uuid rw" > /boot/loader/entries/arch.conf
 
 
 
