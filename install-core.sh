cc54f9d65f7c3fdc3737c12dbe4ff16389a37b71
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-11-23 23:41:25 +0100

    modified script to meet bash standards

--- install-core.sh
+++ install-core.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-hostname=($1)
-username=($2)
-uefipart=($3)
-rootpart=($4)
+hostname=$1
+username=$2
+uefi_part=$3
+root_part=$4
 
 
 
 # Set the hostname
-    echo  $hostname > /etc/hostname
+    echo  ${hostname} > /etc/hostname
 
 
 # Set le locales and the keymap
@@ -27,12 +27,12 @@ rootpart=($4)
 
 # Configure boot manager
     pacman -S efibootmgr
-    mount $uefipart /boot
+    mount ${uefi_part} /boot
     bootctl --path=/boot install
 
     echo -e "timeout=3\ndefault=arch" > /boot/loader/loader.conf
-    rootpart_uuid=$(blkid $rootpart | cut -f2 -d\")
-    echo -e "title  arch\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=UUID=$rootpart_uuid rw" > /boot/loader/entries/arch.conf
+    root_part_uuid=$(blkid ${root_part} | cut -f2 -d\")
+    echo -e "title  arch\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=UUID=$root_part_uuid rw" > /boot/loader/entries/arch.conf
 
 
 # Set root password
@@ -41,16 +41,16 @@ rootpart=($4)
 
 
 # add the user to wheel group (sudoers)
-    useradd -m -G wheel -s /bin/bash $username
+    useradd -m -G wheel -s /bin/bash ${username}
     sed -i "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g" /etc/sudoers
     # Set user password
-        echo Enter $username password:
-        passwd $username
+        echo Enter ${username} password:
+        passwd ${username}
 
 
 # Move the git repo into the user's home directory
     pushd `dirname $0` > /dev/null
     git_repo_path=`pwd`
     popd > /dev/null
-    mv $git_repo_path /home/$username
-    chown -R $username:$username /home/$username/
+    mv ${git_repo_path} /home/${username}
+    chown -R ${username}:${username} /home/${username}/
