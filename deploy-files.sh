ef65eecde9d0b7b2f8d17248e31e6a4dfb264058
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-11-30 20:11:29 +0100

    removed useless sudo, call this script with root rights

--- deploy-files.sh
+++ deploy-files.sh
@@ -17,8 +17,8 @@ shopt -s dotglob #To be able to copy dotfiles
 
 cp -R config-files/homedir/* $usernameHome
 
-sudo cp $otherConfigFilesDir/slim/slim.conf /etc/
-sudo mkdir -p /usr/share/slim/themes/ && sudo cp -R $otherConfigFilesDir/slim/slim-minimal/ /usr/share/slim/themes/ 
-sudo mkdir -p /usr/share/gtk-3.0/ && sudo cp -R $otherConfigFilesDir/gtk-theme/settings.ini /usr/share/gtk-3.0/
+cp $otherConfigFilesDir/slim/slim.conf /etc/
+mkdir -p /usr/share/slim/themes/ && cp -R $otherConfigFilesDir/slim/slim-minimal/ /usr/share/slim/themes/ 
+mkdir -p /usr/share/gtk-3.0/ && cp -R $otherConfigFilesDir/gtk-theme/settings.ini /usr/share/gtk-3.0/
 
-sudo cp scripts/* /bin/
+cp scripts/* /bin/
