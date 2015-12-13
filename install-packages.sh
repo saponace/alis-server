f8813a2ced661e80337427081e253ba88af4f63b
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-12-13 18:10:47 +0100

    corrected typo

--- install-packages.sh
+++ install-packages.sh
@@ -26,7 +26,7 @@ install_transmission
 }
 
 install_yaourt(){
-    su -c echo -e "$'[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/$arch' >> /etc/pacman.conf"
+    su -c "echo -e $'[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch' >> /etc/pacman.conf"
     sudo pacman -Syu yaourt
 }
 
