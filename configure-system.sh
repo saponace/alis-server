42a5d44d373327e19e99751b6ce201cb823636ed
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2015-12-13 19:04:18 +0100

    added a su -c i forgot

--- configure-system.sh
+++ configure-system.sh
@@ -79,8 +79,8 @@ set_misc(){
 }
 
 set_gtk_theme() {
-    sudo echo -e "gtk-icon-theme-name = \"Arc-Darker\"\ngtk-theme-name = \"Arc-Darker\"\ngtk-font-name = \"Inconsolata-g 8\"" > /usr/share/gtk-2.0/gtkrc
-    sudo echo -e "[Settings]\ngtk-icon-theme-name = Arc-Darker\ngtk-theme-name = Arc-Darker\ngtk-font-name = Inconsolata-g 8" > /usr/share/gtk-3.0/settings.ini
+    su -c "echo -e 'gtk-icon-theme-name = \"Arc-Darker\"\ngtk-theme-name = \"Arc-Darker\"\ngtk-font-name = \"Inconsolata-g 8\"' > /usr/share/gtk-2.0/gtkrc"
+    su -c "echo -e '[Settings]\ngtk-icon-theme-name = Arc-Darker\ngtk-theme-name = Arc-Darker\ngtk-font-name = Inconsolata-g 8' > /usr/share/gtk-3.0/settings.ini"
 }
 
 
