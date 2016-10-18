4aae02e70355d391f9bc4b4f977520e5dfa34011
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-10-18 18:42:33 +0200

    Changed default GTK theme from Arc Dark to Arc Darker

--- components/font-and-gtk-theme.sh
+++ components/font-and-gtk-theme.sh
@@ -11,5 +11,5 @@
     ${INSTALL} gtk-theme-arc
 
 # Set this theme and the font for GTK2 and GTK3
-    su -c "echo -e 'gtk-icon-theme-name = \"Arc-Darker\"\ngtk-theme-name = \"Arc-Darker\"\ngtk-font-name = \"Inconsolata-g 8\"' > /usr/share/gtk-2.0/gtkrc"
-    su -c "echo -e '[Settings]\ngtk-icon-theme-name = Arc-Darker\ngtk-theme-name = Arc-Darker\ngtk-font-name = Inconsolata-g 8' > /usr/share/gtk-3.0/settings.ini"
+    su -c "echo -e 'gtk-icon-theme-name = \"Arc-Dark\"\ngtk-theme-name = \"Arc-Dark\"\ngtk-font-name = \"Inconsolata-g 8\"' > /usr/share/gtk-2.0/gtkrc"
+    su -c "echo -e '[Settings]\ngtk-icon-theme-name = Arc-Dark\ngtk-theme-name = Arc-Dark\ngtk-font-name = Inconsolata-g 8' > /usr/share/gtk-3.0/settings.ini"
