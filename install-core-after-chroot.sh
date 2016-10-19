e6f812710eae066fdd90e094a973702b07ae85a4
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-10-20 00:25:18 +0200

    Removed useless linebreak in the systemd-boot entry

--- install-core-after-chroot.sh
+++ install-core-after-chroot.sh
@@ -38,8 +38,7 @@ root_part=$4
     # Configure entry
         root_part_uuid=$(blkid ${root_part} | cut -f2 -d\")
         swap_part_uuid=$(blkid ${swap_part} | cut -f2 -d\")
-        echo -e "title  arch\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\n" > /boot/loader/entries/arch.conf
-        # TODO: Check that mess
+        echo -e "title  arch\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img" > /boot/loader/entries/arch.conf
         echo -e "options cryptdevice=UUID=${root_part_uuid}:root root=/dev/mapper/root rootflags=subvol=ROOT resume=UUID=${swap_part_uuid} quiet rw" >> /boot/loader/entries/arch.conf
 
 
