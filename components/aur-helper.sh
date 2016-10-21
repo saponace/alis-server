e386caf0a3dcb368e813e06e1dafc140eb627af4
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-10-21 23:41:04 +0200

    Added aurtab, for packages tab completion in yaourt

--- components/aur-helper.sh
+++ components/aur-helper.sh
@@ -4,5 +4,10 @@
 #-------------------------------------------------
 
 
-su -c "echo -e $'[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch' >> /etc/pacman.conf"
-sudo pacman -Syu yaourt
+# Yaourt
+    su -c "echo -e $'[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch' >> /etc/pacman.conf"
+    sudo pacman -Syu yaourt
+
+# Yaourt packages tab completion
+    ${INSTALL} aurtab
+
