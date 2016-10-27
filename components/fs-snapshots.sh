21b73e4dc52a6c0606b6459755ffe77da4b5435e
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-10-27 23:47:09 +0200

    Added Btrfs subvolume /home/saponace/.cache so that it is excluded from snapshots

--- components/fs-snapshots.sh
+++ components/fs-snapshots.sh
@@ -13,3 +13,12 @@
     sudo snapper -c home create-config /home
     sudo snapper -c etc create-config /etc
     sudo snapper -c opt create-config /opt
+
+# Prevent /home/${USER}/.cache from being snapshotted
+# Directory may already exist, so we need to make sure
+# it is renamed if it exists before creating the subvolume
+    mv /home/${USER}/.cache /home/${USER}/.cache_tmp
+    sudo btrfs subvolume create /home/${USER}/.cache
+    sudo chown -R ${USER}:${USER} /home/${USER}/.cache
+    mv /home/${USER}/.cache_tmp{*,.*} /home/${USER}/.cache/
+    rmdir /home/${USER}/.cache_tmp
