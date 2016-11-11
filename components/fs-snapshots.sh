9bbbb9e8a8f49e4bdee7702a45a7673695ccfed7
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-11-11 09:22:19 +0100

    Removed ~/.config/google-chrome from snapshotted directories and addde factored function to make easier the process of removing directory from the snapshot lists

--- components/fs-snapshots.sh
+++ components/fs-snapshots.sh
@@ -7,12 +7,14 @@
 # Call snapper before and after each pacman/yaourt call
     ${INSTALL} snap-pac
 
+
 # Setup all the snapper configs (snapshots triggered every hour by default)
     sudo snapper -c root create-config /
     sudo snapper -c root-home create-config /root
     sudo snapper -c home create-config /home
     sudo snapper -c etc create-config /etc
     sudo snapper -c opt create-config /opt
+
 
 # Reduce number of kept snapshots and increase cleanup
 # frequency from all cleanup files
@@ -22,11 +24,21 @@
     done
 
 
-# Prevent /home/${USER}/.cache from being snapshotted
+# Prevent a directory from being snapshotted if it is in a
+# subvolume that is snapshotted with snapper.
+# This function creates a Btrfs subvolume from a directory
 # Directory may already exist, so we need to make sure
 # it is renamed if it exists before creating the subvolume
-    mv /home/${USER}/.cache /home/${USER}/.cache_tmp
-    sudo btrfs subvolume create /home/${USER}/.cache
-    sudo chown -R ${USER}:${USER} /home/${USER}/.cache
-    mv /home/${USER}/.cache_tmp{*,.*} /home/${USER}/.cache/
-    rmdir /home/${USER}/.cache_tmp
+# $1: The directory to convert to a subvolume
+    function create_subvolume_from_dir () {
+        dir=$1
+        owner_of_dir=$(ls -ld ${dir} | awk '{print $3}')
+        group_of_dir=$(ls -ld ${dir} | awk '{print $4}')
+        mv "${dir}" "${dir}_tmp"
+        sudo btrfs subvolume create "${dir}"
+        sudo chown -R ${owner_of_dir}:${group_of_dir} "${dir}"
+        mv "${dir}_tmp"/{*,.*} "${dir}"
+        rmdir "${dir}_tmp"
+    }
+    create_subvolume_from_dir /home/${USER}/.cache
+    create_subvolume_from_dir /home/${USER}/.config/google-chrome
