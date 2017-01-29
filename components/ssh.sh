f34e7e2b7493b2caae5c55843d52f4d6a53cb69e
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-01-29 19:42:13 +0100

    Fixed sshd_config file access

--- components/ssh.sh
+++ components/ssh.sh
@@ -5,24 +5,35 @@
 
 sshd_config_file="/etc/ssh/sshd_config"
 
+
+# Add a configuration to the file /etc/ssh/sshd_config
+# $1: The configuration to add
+function add_sshd_config(){
+    sudo su -c "echo $1 >> ${sshd_config_file}"
+}
+
+
+
 # Install SSH client and daemon
     ${INSTALL} openssh
 
-
 # Enable SSH daemon
     sudo systemctl enable sshd.service
+
+# Initialize sshd_config file
+    sudo su -c "echo '' >> ${sshd_config_file}"
 # Quicker SSH login
-    sudo echo 'UseDNS no' > ${sshd_config_file}
+    add_sshd_config "UseDNS no"
 # Enable SSHFS
-    sudo echo "Subsystem sftp /usr/lib/sftp-server" >> ${sshd_config_file}
+    add_sshd_config "Subsystem sftp /usr/lib/sftp-server"
 
 # Security
     # Whitelist SSH port on the firewall
         sudo ufw allow 22
     # Disable ssh connection to root account
-        sudo echo "PermitRootLogin no" >> ${sshd_config_file}
+        add_sshd_config "PermitRootLogin no"
     # Disable authentication via password, require ssh key
-        sudo echo "PasswordAuthentication no" >> ${sshd_config_file}
+        add_sshd_config "PasswordAuthentication no"
 
     # Add known_hosts file to user's ssh configuration
     if [ -f "${AUTHORIZED_KEYS_FILE}" ]
