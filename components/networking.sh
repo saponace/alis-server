d81436c421b3df68acd07682a12147f106915349
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-01-30 14:00:55 +0100

    Moved systemd networkd start after configuration

--- components/networking.sh
+++ components/networking.sh
@@ -3,12 +3,6 @@
 #-------------------------------------------------
 
 
-# Enabble networking and resolving daemons
-    sudo systemctl enable systemd-networkd
-    sudo systemctl start systemd-networkd
-    sudo systemctl enable systemd-resolved
-    sudo systemctl start systemd-resolved
-
 # Link system resolv.cponf to systemd's resolv.conf
     sudo rm /etc/resolv.conf
     sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
@@ -21,6 +15,12 @@ Address=${static_ip_address}
 Gateway=${lan_gateway_ip_address}
 DNS=${dns_server_ip_address}
 ' >> /etc/systemd/network/10-static-interface.network"
+
+# Enabble networking and resolving daemons
+    sudo systemctl enable systemd-networkd
+    sudo systemctl start systemd-networkd
+    sudo systemctl enable systemd-resolved
+    sudo systemctl start systemd-resolved
 
 
 # Activate Wake On Lan
