20eac36feb35963448ee31f6c2c9a725aa871bc9
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-01-30 15:05:31 +0100

    Added sudo to call su to avoid password prompt

--- components/core.sh
+++ components/core.sh
@@ -8,7 +8,7 @@
 # Set locale and keyboard layout in the terminal and X
     sudo localectl set-keymap fr-latin9
 # Disable terminal bell (actually totally disable internal speaker)
-    su -c "echo 'blacklist pcspkr' > /etc/modprobe.d/nobeep.conf"
+    sudo su -c "echo 'blacklist pcspkr' > /etc/modprobe.d/nobeep.conf"
 # Network Time Protocl Daemon synchronization(synchronize time with internet)
     ${INSTALL} ntp
     sudo systemctl enable ntpd.service
