c65919b0233488fe71c166256495181effff469b
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-02-14 00:22:55 +0100

    Now generate kodi config files scafold before editing

--- components/kodi.sh
+++ components/kodi.sh
@@ -6,11 +6,17 @@
 # Install the package
     ${INSTALL} kodi-rbp-git
 
+# Create config files scafold by starting the application
+    sudo systemctl start kodi
+    sleep 20
+
 # Enable RPC
     kodi_config_file_path="/var/lib/kodi/.kodi/userdata/guisettings.xml"
     sudo sed -i 's|<esallinterfaces default="true">false</esallinterfaces>|<esallinterfaces>true</esallinterfaces>|g' ${kodi_config_file_path}
     sudo sed -i 's|<webserver default="true">false</webserver>|<webserver>true</webserver>|g' ${kodi_config_file_path}
 
+# Stop kodi
+    sudo systemctl stop kodi
 
 # Whitelist kodi RPC port on the firewall
     sudo ufw allow 8080
