744638a46cb386fe1cd5c47655ac58e88ae267c4
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-08-15 18:15:36 +0200

    Tried to not start and stop kodi during install to generate config files since they are not symlinked to a permanent config dir

--- components/kodi.sh
+++ components/kodi.sh
@@ -9,11 +9,5 @@
 # Whitelist kodi RPC port on the firewall
     sudo ufw allow 8080
 
-
-# Create config files scafold by starting the application, and then stop it
-    sudo systemctl start kodi
-    sleep 20
-    sudo systemctl stop kodi
-
-    # Link config directory
-        create_directory_symlink  ${kodi_persistant_config_dir} "/var/lib/kodi/.kodi"
+# Link config directory
+    create_directory_symlink  ${kodi_persistant_config_dir} "/var/lib/kodi/.kodi"
