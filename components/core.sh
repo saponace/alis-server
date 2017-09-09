d19fe9967bb86dd1c3e9723fa511337a1e675955
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-09-09 18:11:37 +0200

    Moved systemd-swap configuration to main config file since overriding config in dediceted directory does not work when called via systemd

--- components/core.sh
+++ components/core.sh
@@ -23,4 +23,4 @@
         # sudo sed -i '$s/$/ zswap.enabled=1/' /boot/cmdline.txt  # Enable zswap
         ${INSTALL} systemd-swap
         sudo systemctl enable systemd-swap
-        create_link "${ADDITIONAL_CONFIG_FILES_DIR}/other/systemd-swap/1.conf" "/etc/systemd/swap.conf.d/"
+        create_link "${ADDITIONAL_CONFIG_FILES_DIR}/other/systemd-swap.conf" "/etc/systemd/"
