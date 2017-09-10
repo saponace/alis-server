fd38b4b6b4c3fb6cf309d00c47176522ed793a3f
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-09-10 13:09:47 +0200

    Moved letsencrypt certbot configuration to file that will be symlinked

--- components/web-server.sh
+++ components/web-server.sh
@@ -15,20 +15,8 @@
 # certbot - Let's encrypt client
     ${INSTALL} certbot-nginx
     # Custom service to renew certificate
-sudo su -c "echo '[Unit]
-Description=Lets Encrypt renewal
-[Service]
-Type=oneshot
-ExecStart=/usr/bin/certbot renew --quiet --agree-tos
-ExecStartPost=/bin/systemctl reload nginx.service' > /etc/systemd/system/certbot.service"
-sudo su -c "echo '[Unit]
-Description=Twice daily renewal of Lets Encrypts certificates
-[Timer]
-OnCalendar=0/12:00:00
-RandomizedDelaySec=1h
-Persistent=true
-[Install]
-WantedBy=timers.target' > /etc/systemd/system/certbot.timer"
+    create_link "${ADDITIONAL_CONFIG_FILES_DIR}/other/systemd-units/certbot.service" "/etc/systemd/system/"
+    create_link "${ADDITIONAL_CONFIG_FILES_DIR}/other/systemd-units/certbot.timer" "/etc/systemd/system/"
     sudo systemctl enable certbot.timer
     # Link config directory
         create_directory_symlink  ${letsencrypt_persistant_config_dir} "/etc/letsencrypt"
