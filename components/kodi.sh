5d1de755045e8f417cfcdd1c9698dc868b846cf6
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2017-09-23 19:23:58 +0200

    Re-added kodi firewall rule

--- components/kodi.sh
+++ components/kodi.sh
@@ -6,9 +6,8 @@
 # Install the package
     ${INSTALL} kodi-rbp-git
 
-# Whitelist kodi RPC port on the firewall
-# Disabled since all traffic goes trough nginx reverse proxy
-    # sudo ufw allow 8080
+Whitelist kodi RPC port on the firewall
+    sudo ufw allow 8080
 
 # Link config directory
     create_directory_symlink  ${kodi_persistant_config_dir} "/var/lib/kodi/.kodi"
