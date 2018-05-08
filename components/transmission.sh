e8936ee42dd3d0bff8a8a67f98675067fd04d827
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-05-08 17:37:37 +0200

    Fixed broken transmission

--- components/transmission.sh
+++ components/transmission.sh
@@ -5,10 +5,11 @@
 ${INSTALL} transmission-cli
 
 # Whitelist transmission RPC port on the firewall
-# Disabled since all traffic goes trough nginx reverse proxy
-    # sudo ufw allow 9091
+    sudo ufw allow 9091
 
 # Copy config file (I copy the file here because I can't manage to make transmission see the file when it's linked
-    target_file="/var/lib/transmission/.config/transmission-daemon/settings.json"
+    target_dir="/var/lib/transmission/.config/transmission-daemon"
+    target_file=${target_dir}"/settings.json"
+    sudo mkdir -p  ${target_dir}
     sudo cp -f "${ADDITIONAL_CONFIG_FILES_DIR}/other/transmission-daemon/settings.json" ${target_file}
     sudo chown transmission:transmission ${target_file}
