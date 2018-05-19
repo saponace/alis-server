f590c238079f00c1c44094e424d9f2d06a275054
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-05-19 23:54:57 +0200

    Docker migration WIP - 2

--- components/utils.sh
+++ components/utils.sh
@@ -2,25 +2,23 @@
 # Install utilities
 #-------------------------------------------------
 
-# Git
-    ${INSTALL} git
-# SSH client and server
-    ${INSTALL} ssh
-# Zip
-    ${INSTALL} unzip
-    ${INSTALL} zip
-# Downloads from the web
-    ${INSTALL} wget
-# Distant and local copy
-    ${INSTALL} rsync
-# List open files by given process
-    ${INSTALL} lsof
-# Delete duplicates files
-    ${INSTALL} rmlint
+# # Git
+#     ${INSTALL} git
+# # SSH client and server
+#     ${INSTALL} ssh
+# # Zip
+#     ${INSTALL} unzip
+#     ${INSTALL} zip
+# # Downloads from the web
+#     ${INSTALL} wget
+# # Distant and local copy
+#     ${INSTALL} rsync
+# # List open files by given process
+#     ${INSTALL} lsof
+# # Delete duplicates files
+#     ${INSTALL} rmlint
 # System monitor tool
-    ${INSTALL} glances
-    # ${INSTALL} python-bottle  # Glances web UI dependency
-    # # Glances web UI daemon
-    #     # ufw enable 61208 # Disabled since all traffic goes trough nginx reverse proxy
-    #     create_link "${ADDITIONAL_CONFIG_FILES_DIR}/other/systemd-units/glances-web.service" "/etc/systemd/system/"
-    #     sudo systemctl enable glances-web
+    declare -A  glances_docker_compose_template_mappings=(
+        ["GLANCES_PORT"]="9006"
+    )
+    process_docker_compose_service glances "$(declare -p glances_docker_compose_template_mappings)"
