7c518bfbed5e720092365652d4a09ec00dd472f1
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-07-03 23:07:08 +0200

    Docker WIP - VPN - 7

--- components/pvr/pvr.sh
+++ components/pvr/pvr.sh
@@ -7,19 +7,16 @@ source components/pvr/variables.sh
 
 # Sonarr: automated TV shows download
     declare -A  sonarr_docker_compose_template_mappings=(
-        ["SONARR_PORT"]="${SONARR_PORT}"
     )
     process_docker_compose_service pvr/sonarr "$(declare -p sonarr_docker_compose_template_mappings)"
 
 # Radarr: automated Movies download
     declare -A  radarr_docker_compose_template_mappings=(
-        ["RADARR_PORT"]="${RADARR_PORT}"
     )
     process_docker_compose_service pvr/radarr "$(declare -p radarr_docker_compose_template_mappings)"
 
 # Jackett: tracker proxy for sonarr and radarr
     declare -A  jackett_docker_compose_template_mappings=(
-        ["JACKETT_PORT"]="${JACKETT_PORT}"
         ["TORRENTS_BLACKHOLE"]="/torrents-blackhole"
     )
     process_docker_compose_service pvr/jackett "$(declare -p jackett_docker_compose_template_mappings)"
--- components/vpn/vpn.sh
+++ components/vpn/vpn.sh
@@ -9,6 +9,7 @@
     docker build -t openvpn-client components/vpn/
 
 # Create credentials file on host machine that will be passed to container
+# TODO: choose location of vpn credentials file and change reference in docker-compose.yml
     # sudo su -c "echo \"${vpn_login}\n${vpn_password}\" > /vpn-credentials"
     # chmod 600 /vpn-credentials
 
@@ -16,9 +17,13 @@
     source components/pvr/variables.sh
     source components/torrent-client/variables.sh
     declare -A  docker_compose_template_mappings=(
-        ["TRANSMISSION_PORT"]="${TORRENT_CLIENT_PORT}"
-        ["SONARR_PORT"]="${SONARR_PORT}"
-        ["RADARR_PORT"]="${RADARR_PORT}"
-        ["JACKETT_PORT"]="${JACKETT_PORT}"
+        ["TRANSMISSION_PORT_INTERNAL"]="${TORRENT_CLIENT_PORT_INTERNAL}"
+        ["TRANSMISSION_PORT_EXTERNAL"]="${TORRENT_CLIENT_PORT_EXTERNAL}"
+        ["SONARR_PORT_INTERNAL"]="${SONARR_PORT_INTERNAL}"
+        ["SONARR_PORT_EXTERNAL"]="${SONARR_PORT_EXTERNAL}"
+        ["RADARR_PORT_INTERNAL"]="${RADARR_PORT_INTERNAL}"
+        ["RADARR_PORT_EXTERNAL"]="${RADARR_PORT_EXTERNAL}"
+        ["JACKETT_PORT_INTERNAL"]="${JACKETT_PORT_INTERNAL}"
+        ["JACKETT_PORT_EXTERNAL"]="${JACKETT_PORT_EXTERNAL}"
     )
     process_docker_compose_service vpn/vpn "$(declare -p docker_compose_template_mappings)"
