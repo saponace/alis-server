6128f64ff63a1f4e153edade6b16f4ba90c1923a
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-07-22 00:18:23 +0200

    Home assistant - 2

--- components/home-assistant/home-assistant.sh
+++ components/home-assistant/home-assistant.sh
@@ -5,4 +5,4 @@
 
 declare -A  docker_compose_template_mappings=(
 )
-process_docker_compose_service homeassistant/homeassistant "$(declare -p docker_compose_template_mappings)"
+process_docker_compose_service home-assistant/home-assistant "$(declare -p docker_compose_template_mappings)"
