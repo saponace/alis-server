8ec693329808643105c76c8ee8561c5dd08acf7e
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-07-22 14:58:57 +0200

    Media server WIP - 3

--- components/media-center-2/media-center-2.sh
+++ components/media-center-2/media-center-2.sh
@@ -10,3 +10,11 @@ process_docker_compose_service media-center-2/plex "$(declare -p plex_docker_com
 declare -A emby_docker_compose_template_mappings=(
 )
 process_docker_compose_service media-center-2/emby "$(declare -p emby_docker_compose_template_mappings)"
+
+declare -A ombi_docker_compose_template_mappings=(
+)
+process_docker_compose_service media-center-2/ombi "$(declare -p ombi_docker_compose_template_mappings)"
+
+declare -A tautulli_docker_compose_template_mappings=(
+)
+process_docker_compose_service media-center-2/tautulli "$(declare -p tautulli_docker_compose_template_mappings)"
