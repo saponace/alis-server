73f75622876934d86197774d9df2423a0e9155eb
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-08-20 02:35:33 +0200

    Added dhcpcd initial config to get access to the internet during install

--- configure-system.sh
+++ configure-system.sh
@@ -51,9 +51,25 @@ function install_component (){
   echo "" 2>&1 | tee -a ${LOG_FILE}
 }
 
+function enable_networking (){
+  echo "Enabling networking ..."
+  sudo dhcpcd
+  while [ "$var1" != "end" ]
+  do
+      pingtime=$(ping -w 1 google.com | grep ttl)
+      if [ "$pingtime" = "" ]
+      then
+          sleep 2
+      else
+          break
+      fi
+  done
+  echo "Done !"
+}
 
 
 
+enable_networking
 source common-functions.sh
 install_component networking
 install_component aur-helper
