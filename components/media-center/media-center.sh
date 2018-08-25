8d7a5a63006ab0fe45e9dfb115920d0913a812db
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-08-25 21:03:24 +0200

    Forgot to migrate script references after script name change

--- components/media-center/media-center.sh
+++ components/media-center/media-center.sh
@@ -5,16 +5,16 @@
 
 declare -A plex_docker_compose_template_mappings=(
 )
-process_docker_compose_service media-center-2/plex "$(declare -p plex_docker_compose_template_mappings)"
+process_docker_compose_service media-center/plex "$(declare -p plex_docker_compose_template_mappings)"
 
 declare -A emby_docker_compose_template_mappings=(
 )
-process_docker_compose_service media-center-2/emby "$(declare -p emby_docker_compose_template_mappings)"
+process_docker_compose_service media-center/emby "$(declare -p emby_docker_compose_template_mappings)"
 
 declare -A ombi_docker_compose_template_mappings=(
 )
-process_docker_compose_service media-center-2/ombi "$(declare -p ombi_docker_compose_template_mappings)"
+process_docker_compose_service media-center/ombi "$(declare -p ombi_docker_compose_template_mappings)"
 
 declare -A tautulli_docker_compose_template_mappings=(
 )
-process_docker_compose_service media-center-2/tautulli "$(declare -p tautulli_docker_compose_template_mappings)"
+process_docker_compose_service media-center/tautulli "$(declare -p tautulli_docker_compose_template_mappings)"
