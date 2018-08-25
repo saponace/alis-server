0cdd88dea0ebdc53c67f8ea2f9019e7f491b318d
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-08-25 21:02:00 +0200

    typo

--- configure-system.sh
+++ configure-system.sh
@@ -88,7 +88,7 @@ install_component vpn-client
 install_component reverse-proxy
 install_component torrenting-client
 # install_component web-server
-install_component media-center-
+install_component media-center
 install_component home-assistant
 install_component system-monitoring
 install_component pvr
