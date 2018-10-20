85f630d76e7524159aa267147ff5b24b74571e7b
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-10-20 16:55:43 +0200

    fix: file permission

--- components/docker/docker.sh
+++ components/docker/docker.sh
@@ -26,9 +26,9 @@ sudo systemctl enable docker
 mkdir -p /opt
 docker_compose_file="/opt/docker-compose.yml"
 
-cat components/docker/base.yml >> ${docker_compose_file}
+sudo cat components/docker/base.yml >> ${docker_compose_file}
 
 for part in ${TEMP_DOCKER_COMPOSE_PARTS_DIR}/*; do
-    cat ${part} >> ${docker_compose_file}
+    sudo cat ${part} >> ${docker_compose_file}
 done
 
