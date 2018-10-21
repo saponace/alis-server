dc9663fc239f82caf33ca3680b0edc344a4f81cd
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-10-21 21:47:25 +0200

    fix: netdata can now get other containers names (+switched to new official netdata dockerhub image)

--- components/docker/docker.sh
+++ components/docker/docker.sh
@@ -7,11 +7,6 @@ ${INSTALL} docker docker-compose
 sudo systemctl enable docker
 
 
-# Docker-proxy-ACL: Access layer for docker.sock (used by netdata)
-    declare -A  docker_proxy_acl_docker_compose_template_mappings=(
-   )
-    process_docker_compose_service docker/docker-proxy-acl "$(declare -p docker_proxy_acl_docker_compose_template_mappings)"
-
 # Watchtower: Watch containers for new versions and seamlessly update
     declare -A  watchtower_docker_compose_template_mappings=(
     )
@@ -23,19 +18,19 @@ sudo systemctl enable docker
     process_docker_compose_service docker/portainer "$(declare -p portainer_docker_compose_template_mappings)"
 
 
+
 # Merge all docker-compose parts into final docker-compose.yml
    mkdir -p /opt
    docker_compose_dir="/opt/server-apps-suite"
    docker_compose_file="${docker_compose_dir}/docker-compose.yml"
-
    sudo mkdir ${docker_compose_dir}
    sudo chown ${username}:${username} ${docker_compose_dir}
 
    cat components/docker/base.yml > ${docker_compose_file}
-
    for part in ${TEMP_DOCKER_COMPOSE_PARTS_DIR}/*; do
       cat ${part} >> ${docker_compose_file}
    done
+
 
 
 # Create systemd unit file and start docker-compose at bootup
