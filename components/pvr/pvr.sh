4b14a69f54239f1da1c47abf7df6d0169ff526a8
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-11-07 23:41:50 +0100

    feat: add lidarr: automated music download

--- components/pvr/pvr.sh
+++ components/pvr/pvr.sh
@@ -7,10 +7,16 @@
     )
     process_docker_compose_service pvr/sonarr "$(declare -p sonarr_docker_compose_template_mappings)"
 
-# Radarr: automated Movies download
+# Radarr: automated movies download
     declare -A  radarr_docker_compose_template_mappings=(
     )
     process_docker_compose_service pvr/radarr "$(declare -p radarr_docker_compose_template_mappings)"
+
+# Lidarr: automated music download
+# I know, music is not video (as in PVR), but I thought it fits well here
+    declare -A  lidarr_docker_compose_template_mappings=(
+    )
+    process_docker_compose_service pvr/lidarr "$(declare -p lidarr_docker_compose_template_mappings)"
 
 # Jackett: tracker proxy for sonarr and radarr
     declare -A  jackett_docker_compose_template_mappings=(
