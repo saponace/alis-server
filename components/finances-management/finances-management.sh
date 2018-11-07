7ae0f44c68bd06a79ffe24cd75c10dbdab6378ad
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-11-07 23:45:34 +0100

    fix: made firefly-iii app and db versions same for docker-compose and install-time docker container used to initialize te database

--- components/finances-management/finances-management.sh
+++ components/finances-management/finances-management.sh
@@ -7,8 +7,8 @@ db_username="firefly_db"
 db_pwd="firefly_db_secret"
 app_key=$( cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 )
 data_dir="/firefly-iii"
-firefly_iii_version=latest
-mariadb_version=latest
+firefly_iii_version="release-4.7.8"
+mariadb_version="10.3.10"
 
 declare -A  firefly_iii_app_docker_compose_template_mappings=(
     ["FIREFLY_DATA"]=${data_dir}
@@ -16,7 +16,7 @@ declare -A  firefly_iii_app_docker_compose_template_mappings=(
     ["DB_USERNAME"]=${db_username}
     ["DB_PWD"]=${db_pwd}
     ["APP_KEY"]=${app_key}
-    ["FIREFLY_III_VERSION"]=${firefly_iii_version}
+    ["VERSION"]=${firefly_iii_version}
 )
 process_docker_compose_service finances-management/firefly-iii-app "$(declare -p firefly_iii_app_docker_compose_template_mappings)"
 
@@ -24,7 +24,7 @@ declare -A  firefly_iii_db_docker_compose_template_mappings=(
     ["DB_NAME"]=${db_name}
     ["DB_USERNAME"]=${db_username}
     ["DB_PWD"]=${db_pwd}
-    ["DATABASE_VERSION"]=${mariadb_version}
+    ["VERSION"]=${mariadb_version}
 )
 process_docker_compose_service finances-management/firefly-iii-db "$(declare -p firefly_iii_db_docker_compose_template_mappings)"
 
