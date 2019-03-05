6c5aee357c3d04e620c4ea7ed8608689c04db6b4
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2019-03-05 21:32:42 +0100

    Task manager - WIP 2

--- components/task-manager/task-manager.sh
+++ components/task-manager/task-manager.sh
@@ -3,16 +3,16 @@
 #-------------------------------------------------
 
 data_dir="/wekan"
-wekan_version="v2.7"
+wekan_version="v2.37"
 mongo_version="3.2.21"
 
 declare -A  wekan_app_docker_compose_template_mappings=(
     ["VERSION"]=${wekan_version}
 )
-process_docker_compose_service finances-management/wekan-app "$(declare -p wekan_app_docker_compose_template_mappings)"
+process_docker_compose_service task-manager/wekan-app "$(declare -p wekan_app_docker_compose_template_mappings)"
 
 declare -A  wekan_db_docker_compose_template_mappings=(
 ["WEKAN_DATA"]=${data_dir}
     ["VERSION"]=${mongo_version}
 )
-process_docker_compose_service finances-management/wekan-db "$(declare -p wekan_db_docker_compose_template_mappings)"
+process_docker_compose_service task-manager/wekan-db "$(declare -p wekan_db_docker_compose_template_mappings)"
