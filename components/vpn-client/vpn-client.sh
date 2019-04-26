cf4ada9d83b779805b32157f3cb9220ff764164e
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2019-04-26 23:17:26 +0200

    chore: PIA client image build from local script to docker hub

--- components/vpn-client/vpn-client.sh
+++ components/vpn-client/vpn-client.sh
@@ -3,16 +3,6 @@
 #-------------------------------------------------
 
 
-# Building docker image
-    # Build dependency
-        ${INSTALL} docker
-        sudo systemctl start docker
-    sudo docker build -t openvpn-client components/vpn-client/docker-build
-
-# Create credentials file on host machine that will be passed to container
-    sudo su -c "echo \"${vpn_login}\n${vpn_password}\" > /pia-credentials"
-    sudo chmod 600 /pia-credentials
-
 # Build docker-compose definition
     declare -A  docker_compose_template_mappings=(
     )
