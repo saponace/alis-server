99a8c8dcc050009db1f5f18e08d17157c27a7ae6
Remi Somdecoste-Lespoune <saponace@gmail.com>
2019-08-19 18:42:01 +0000

    feat: Added filebrowser database persistance to keep credentials and co. after container prune

--- components/file-browser-web/file-browser-web.sh
+++ components/file-browser-web/file-browser-web.sh
@@ -7,3 +7,7 @@
 declare -A  docker_compose_template_mappings=(
 )
 process_docker_compose_service file-browser-web/filebrowser "$(declare -p docker_compose_template_mappings)"
+
+# Create and empty file to prevent docker from creating a directory instead
+    sudo mkdir -p ${CONTAINERS_DATA_DIR}/filebrowser
+    sudo touch ${CONTAINERS_DATA_DIR}/filebrowser/database.db
