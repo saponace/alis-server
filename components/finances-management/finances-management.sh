075b5759bf370680c475e80bd854f054d7440dc8
Remi Somdecoste-Lespoune <saponace@gmail.com>
2019-08-19 14:25:26 +0000

    feat: containers versions bump

--- components/finances-management/finances-management.sh
+++ components/finances-management/finances-management.sh
@@ -7,8 +7,8 @@ db_username="firefly_db"
 db_pwd="firefly_db_secret"
 app_key=$( cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 )
 data_dir="${CONTAINERS_DATA_DIR}/firefly-iii"
-firefly_iii_version="release-4.7.15"
-mariadb_version="10.3.10"
+firefly_iii_version="release-4.8.0.2"
+postgres_version="12"
 
 declare -A  firefly_iii_app_docker_compose_template_mappings=(
     ["FIREFLY_DATA"]=${data_dir}
@@ -21,62 +21,8 @@ declare -A  firefly_iii_app_docker_compose_template_mappings=(
 process_docker_compose_service finances-management/firefly-iii-app "$(declare -p firefly_iii_app_docker_compose_template_mappings)"
 
 declare -A  firefly_iii_db_docker_compose_template_mappings=(
-    ["DB_NAME"]=${db_name}
     ["DB_USERNAME"]=${db_username}
     ["DB_PWD"]=${db_pwd}
-    ["VERSION"]=${mariadb_version}
+    ["VERSION"]=${postgres_version}
 )
 process_docker_compose_service finances-management/firefly-iii-db "$(declare -p firefly_iii_db_docker_compose_template_mappings)"
-
-
-# Create/update the database
-    sudo systemctl start docker
-    # Start the temporary databse container
-        sudo docker run -d \
-        --name firefly_iii_db \
-        -v ${data_dir}/db:/var/lib/mysql \
-        -e MYSQL_DATABASE=${db_name} \
-        -e MYSQL_USER=${db_username} \
-        -e MYSQL_PASSWORD=${db_pwd} \
-        -e MYSQL_RANDOM_ROOT_PASSWORD=yes \
-        mariadb:${mariadb_version}
-
-    # Start the temporary application container
-        sudo docker run -d \
-        --name firefly_iii_app \
-        --link firefly_iii_db \
-        -v ${data_dir}/export:/var/www/firefly-iii/storage/export \
-        -v ${data_dir}/upload:/var/www/firefly-iii/storage/upload \
-        -e FF_DB_HOST=firefly_iii_db \
-        -e FF_DB_NAME=${db_name} \
-        -e FF_DB_USER=${db_username} \
-        -e FF_DB_PASSWORD=${db_pwd} \
-        -e FF_APP_KEY=${app_key} \
-        -e FF_APP_ENV=local \
-        -p 80:80 \
-        jc5x/firefly-iii:${firefly_iii_version}
-
-
-        # Stop and restart both containers
-          sudo docker container stop firefly_iii_app
-          sudo docker container stop firefly_iii_db
-          sudo docker container start firefly_iii_db
-          sudo docker container start firefly_iii_app
-        # Wait for a while to make sure both containers are started
-          sleep 10
-
-
-    # Execute the databse creation/migration commands
-    # Calling a HTTP GET on __FIREFLY_URL__/install should be enough, but it does not seem to work
-        echo "Executing database creation/migration ..."
-        sudo docker exec -it firefly_iii_app php artisan migrate --seed
-        sudo docker exec -it firefly_iii_app php artisan firefly:upgrade-database
-        sudo docker exec -it firefly_iii_app php artisan firefly:verify
-        sudo docker exec -it firefly_iii_app php artisan passport:install > /dev/null
-        echo "Database creation/migration executed !"
-
-    # Stop the temporary containers
-        sudo docker container stop firefly_iii_app
-        sudo docker rm firefly_iii_app
-        sudo docker container stop firefly_iii_db
-        sudo docker rm firefly_iii_db
