eaf467d88a2a73cd78d3686df7297bc199165d3a
Remi Somdecoste-Lespoune <saponace@gmail.com>
2019-08-20 17:36:24 +0000

    fix: bitwarden_rs part name typo

--- components/password-manager/password-manager.sh
+++ components/password-manager/password-manager.sh
@@ -6,4 +6,4 @@
 # Create docker compose component
 declare -A  docker_compose_template_mappings=(
 )
-process_docker_compose_service password-manager/bitwarden "$(declare -p docker_compose_template_mappings)"
+process_docker_compose_service password-manager/bitwardenrs "$(declare -p docker_compose_template_mappings)"
