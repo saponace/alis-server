dcc52d6509f13e0c764cd0470d08fa04a613a0ac
Remi Somdecoste-Lespoune <saponace@gmail.com>
2019-08-21 00:28:36 +0000

    fix: (Re)add credentials file creation that was mistalenly removed

--- components/vpn-client/vpn-client.sh
+++ components/vpn-client/vpn-client.sh
@@ -7,3 +7,7 @@
     declare -A  docker_compose_template_mappings=(
     )
     process_docker_compose_service vpn-client/pia-client "$(declare -p docker_compose_template_mappings)"
+
+# Create credentials file on host machine that will be passed to container
+    sudo su -c "echo \"${vpn_login}\n${vpn_password}\" > /pia-credentials"
+    sudo chmod 600 /pia-credentials
