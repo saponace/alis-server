7bebe2d4ae0be3cd632c290751e9423a1063ad56
Rémi Somdecoste-Lespoune <sapoance@gmail.com>
2020-01-16 19:02:22 +0100

    feat: replaced emby by jellyfin

--- components/media-server/media-server.sh
+++ components/media-server/media-server.sh
@@ -3,9 +3,9 @@
 #-------------------------------------------------
 
 
-declare -A emby_docker_compose_template_mappings=(
+declare -A jellyfin_docker_compose_template_mappings=(
 )
-process_docker_compose_service media-server/emby "$(declare -p emby_docker_compose_template_mappings)"
+process_docker_compose_service media-server/jellyfin "$(declare -p jellyfin_docker_compose_template_mappings)"
 
 declare -A ombi_docker_compose_template_mappings=(
 )
