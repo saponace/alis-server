83f72ea6b4d3ee9b4b4fd9d6557ffd5ed5c8f6eb
Remi Somdecoste-Lespoune <saponace@gmail.com>
2020-05-01 13:51:22 +0200

    feat: add public filebrowser instance

--- components/file-browser-web/file-browser-web.sh
+++ components/file-browser-web/file-browser-web.sh
@@ -2,12 +2,32 @@
 # File browser with web interface ala Google drive
 #-------------------------------------------------
 
+filebrowser_version="v2.0.12"
 
-# Create docker compose component
-declare -A  docker_compose_template_mappings=(
-)
-process_docker_compose_service file-browser-web/filebrowser "$(declare -p docker_compose_template_mappings)"
+# Private filebrowser instance
+    filebrowser_data_dir=${CONTAINERS_DATA_DIR}/filebrowser
 
-# Create and empty file to prevent docker from creating a directory instead
-    sudo mkdir -p ${CONTAINERS_DATA_DIR}/filebrowser
-    sudo touch ${CONTAINERS_DATA_DIR}/filebrowser/database.db
+    declare -A  filebrowser_private_docker_compose_template_mappings=(
+        ["MNT_DIR"]="/mnt"
+        ["DATA_DIR"]=${filebrowser_data_dir}
+        ["VERSION"]=${filebrowser_version}
+    )
+    process_docker_compose_service file-browser-web/filebrowser "$(declare -p filebrowser_private_docker_compose_template_mappings)"
+
+    # Create and empty file to prevent docker from creating a directory instead
+        sudo mkdir -p ${filebrowser_data_dir}
+        sudo touch ${filebrowser_data_dir}/database.db
+
+# Public filebrowser instance
+    filebrowser_public_data_dir=${CONTAINERS_DATA_DIR}/filebrowser-public
+
+    declare -A  filebrowser_public_docker_compose_template_mappings=(
+        ["MNT_DIR"]="/mnt/public"
+        ["DATA_DIR"]=${filebrowser_public_data_dir}
+        ["VERSION"]=${filebrowser_version}
+    )
+    process_docker_compose_service file-browser-web/filebrowser-public "$(declare -p filebrowser_public_docker_compose_template_mappings)"
+
+    # Create and empty file to prevent docker from creating a directory instead
+        sudo mkdir -p ${filebrowser_public_data_dir}
+        sudo touch ${filebrowser_public_data_dir}/database.db
