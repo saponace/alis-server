b84d9a4eae19060ee4ea7caa85c16abd321bd9a0
Remi Somdecoste-Lespoune <saponace@gmail.com>
2020-05-22 13:04:13 +0200

    fix: fixed fail2ban rules and cleaned jails configs

--- components/security/security.sh
+++ components/security/security.sh
@@ -6,23 +6,19 @@
 
 # Firewall
    ${INSTALL} ufw
-   sudo ufw enable
+   sudo ufw --force enable
    sudo systemctl enable ufw.service
 
 # Fail2ban
    ${INSTALL} fail2ban
-   sudo_create_link ${COMPONENTS_DIR}/security/config/fail2ban/jail.local /etc/fail2ban/
-   sudo_create_link ${COMPONENTS_DIR}/security/config/fail2ban/filter.d/traefik.conf /etc/fail2ban/filter.d
-   sudo_create_link ${COMPONENTS_DIR}/security/config/fail2ban/filter.d/traefik-auth.conf /etc/fail2ban/filter.d
-   sudo_create_link ${COMPONENTS_DIR}/security/config/fail2ban/filter.d/traefik-botsearch.conf /etc/fail2ban/filter.d
-   sudo_create_link ${COMPONENTS_DIR}/security/config/fail2ban/jail.d/traefik.conf /etc/fail2ban/jail.d
+   sudo cp -rT ${COMPONENTS_DIR}/security/config/fail2ban/ /etc/fail2ban/
 
    # Cloudflare connection configto update Cloudflare firewall
       # Fail2ban Cloudflare action dependency
          ${INSTALL} curl
-      # Not sure if /etc/fail2ban/action.d/ files are subject to modification by fail2ban ... If yes, then this will
+      # Not sure if /etc/fail2ban/action.d/ files are subject to modification by fail2ban ... If yes, then this could
       # break after a while
-      sudo sed -i "s/cftoken =/cftoken = ${cloudflare_api_key}/g" /etc/fail2ban/action.d/cloudflare.conf
-      sudo sed -i "s/cfuser =/cfuser = ${cloudflare_email}/g" /etc/fail2ban/action.d/cloudflare.conf
+      sudo sed -i "s/cftoken =.*$/cftoken = ${cloudflare_api_key}/g" /etc/fail2ban/action.d/cloudflare.conf
+      sudo sed -i "s/cfuser =.*$/cfuser = ${cloudflare_email}/g" /etc/fail2ban/action.d/cloudflare.conf
 
    sudo systemctl enable fail2ban
